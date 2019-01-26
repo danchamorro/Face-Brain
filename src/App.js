3dfb2979165098be96d7801311e0f5ad9458573e
Dan Chamorro <dan@danchamorro.com>
2019-01-26 15:34:17 -0500

    Configured Clarafi API tested and gettign back Face Recognition.

--- src/App.js
+++ src/App.js
@@ -26,29 +26,27 @@ const particlesOptions = {
 
 class App extends Component {
   state = {
-    input: ""
+    input: "",
+    imageUrl: ""
   };
 
   onInputChange = event => {
-    console.log(event.target.value);
+    this.setState({ input: event.target.value });
   };
 
   onButtonSubmit = () => {
-    console.log("click");
-    app.models
-      .predict(
-        "a403429f2ddf4b49b307e318f00e528b",
-        "https://samples.clarifai.com/face-det.jpg"
-      )
-      .then(
-        function(response) {
-          // do something with response
-          console.log(response);
-        },
-        function(err) {
-          // there was an error
-        }
-      );
+    this.setState({ imageUrl: this.state.input });
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function(response) {
+        // do something with response
+        console.log(
+          response.outputs[0].data.regions[0].region_info.bounding_box
+        );
+      },
+      function(err) {
+        // there was an error
+      }
+    );
   };
 
   render() {
@@ -62,7 +60,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        <FaceRecognition />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
