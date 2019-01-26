1aed013bf9069afa424327cff08b023550d9c48d
Dan Chamorro <dan@danchamorro.com>
2019-01-26 14:34:28 -0500

    Added Onbutton Submit

--- src/App.js
+++ src/App.js
@@ -27,6 +27,10 @@ class App extends Component {
     console.log(event.target.value);
   };
 
+  onButtonSubmit = () => {
+    console.log("click");
+  };
+
   render() {
     return (
       <div className="App">
@@ -34,7 +38,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
         {/* <FaceRecognition /> */}
       </div>
     );
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ImageLinkForm.css";
 
-export default function ImageLinkForm({ onInputChange }) {
+export default function ImageLinkForm({ onInputChange, onButtonSubmit }) {
   return (
     <div>
       <p className="f3">
@@ -14,7 +14,10 @@ export default function ImageLinkForm({ onInputChange }) {
             type="text"
             onChange={onInputChange}
           />
-          <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
+          <button
+            className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+            onClick={onButtonSubmit}
+          >
             Detect
           </button>
         </div>
