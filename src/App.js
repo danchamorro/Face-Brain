22694d40f3d140aa50cabdf7062098be3a86d92d
Dan Chamorro <dan@danchamorro.com>
2019-01-26 15:08:33 -0500

    Added Clarifai API and ENV files for key.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import Particles from "react-particles-js";
+import Clarifai from "clarifai";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import "./App.css";
+
+const app = new Clarifai.App({
+  apiKey: process.env.REACT_APP_CLARIFAI_KEY
+});
 
 const particlesOptions = {
   particles: {
@@ -29,6 +35,20 @@ class App extends Component {
 
   onButtonSubmit = () => {
     console.log("click");
+    app.models
+      .predict(
+        "a403429f2ddf4b49b307e318f00e528b",
+        "https://samples.clarifai.com/face-det.jpg"
+      )
+      .then(
+        function(response) {
+          // do something with response
+          console.log(response);
+        },
+        function(err) {
+          // there was an error
+        }
+      );
   };
 
   render() {
@@ -42,7 +62,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        {/* <FaceRecognition /> */}
+        <FaceRecognition />
       </div>
     );
   }
