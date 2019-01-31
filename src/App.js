142ff4647069a66efd2b2511e679ebb4c002fb4b
Dan Chamorro <dan@danchamorro.com>
2019-01-30 19:15:02 -0500

    Changed fetch routes to match Heorku db location

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch("http://localhost:5000/imageurl", {
+    fetch("https://secret-oasis-49482.herokuapp.com/imageurl", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -88,7 +88,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch("http://localhost:5000/image", {
+          fetch("https://secret-oasis-49482.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
